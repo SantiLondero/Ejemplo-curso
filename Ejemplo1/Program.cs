26f3ae17c60171d27a9836765587a17b60399438
SantiLondero <santilondero@gmail.com>
2023-03-16 17:13:14 -0300

    primer subida...

---
+++ Ejemplo1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Ingrese un número: ");
+            n = int.Parse(Console.ReadLine());
+            if (n > 0)
+            {
+                Console.WriteLine("Positivo");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine("Negativo");
+            }
+            else
+                Console.WriteLine("Nulo");
+
+            Console.ReadKey();
+        }
+    }
+}
